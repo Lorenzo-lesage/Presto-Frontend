f04657fe465ef8f12ac265ad7334cf7b39910f01
lorenzo lesage <lollolesage99@gmail.com>
2024-12-19 19:10:41 +0100

    aggiunta sezione le ultime auto e alcune funzionalità JS

--- main.js
+++ main.js
@@ -24,13 +24,103 @@ function createIntervalCustom(finalNumber, element, speed) {
             counter++;
             element.innerHTML = `${counter} +`
         } else {
-            console.timeLog("ho terminato")
             clearInterval(intervalloCustom)
         }
     }, speed);
     
 }
 
-createIntervalCustom(100, autoNum, 50)
-createIntervalCustom(250, clientNum, 50)
-createIntervalCustom(30, accessoriNum, 200)
+// createIntervalCustom(100, autoNum, 50)
+// createIntervalCustom(250, clientNum, 50)
+// createIntervalCustom(30, accessoriNum, 200)
+
+
+// BUTTON NAV COLLAPSE AT O Y
+let buttonNav = document.querySelector("#buttonNav")
+
+buttonNav.addEventListener("click", () => {
+    // Controlla se la navbar ha la classe 'nav-scrolled'
+    if (navbar.classList.contains("nav-scrolled") && scrollY === 0) {
+        // Se la classe esiste, rimuovila
+        navbar.classList.remove("nav-scrolled");
+    } else {
+        // Altrimenti, aggiungila
+        navbar.classList.add("nav-scrolled");
+    }
+});
+
+
+// OBSERVER
+
+let isNotEntered = true
+let observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+        if(entry.isIntersecting && isNotEntered){
+            createIntervalCustom(100, autoNum, 100);
+            createIntervalCustom(250, clientNum, 50);
+            createIntervalCustom(30, accessoriNum, 200);
+            isNotEntered = false    
+        }
+    });
+})
+
+observer.observe(autoNum)
+
+
+
+let cars = [
+    {
+        Marca: "Toyota",
+        Modello: "Corolla",
+        Prezzo: "28.000",
+        img: "https://picsum.photos/200"
+    },
+    {
+        Marca: "BMW",
+        Modello: "Serie 3",
+        Prezzo: "60.000",
+        img: "https://picsum.photos/201"
+    },
+    {
+        Marca: "Tesla",
+        Modello: "Model 3",
+        Prezzo: "60.000",
+        img: "./media/tesla.png"
+    },
+    {
+        Marca: "Volkswagen",
+        Modello: "Golf",
+        Prezzo: "35.000",
+        img: "./media/golf.png"
+    },
+    {
+        Marca: "Audi",
+        Modello: "Q5",
+        Prezzo: "70.000",
+        img: "./media/audi.png"
+    }
+
+]
+
+let cardsWrapper  = document.querySelector("#cardsWrapper")
+
+cars.forEach( (car, i) => {
+    if(i >= cars.length - 3){
+        let div = document.createElement("div")
+        div.classList.add("col-12", "col-md-3")
+        div.innerHTML = `
+                            <div class="card my-2 p-2 text-center">
+                                <img src=${car.img} class="card-img-top img-last-cars" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Marca: ${car.Marca}</h5>
+                                    <p class="card-text">Modello: ${car.Modello}</p>
+                                    <p class="card-text">Prezzo: ${car.Prezzo}</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+        `
+        cardsWrapper.appendChild(div) 
+    }
+    
+})
